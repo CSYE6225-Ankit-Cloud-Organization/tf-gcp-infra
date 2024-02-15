6e254e550ec401ba583e2d5c76ac71ee12f530a1
Ankit Goyal <114370700+goyalankit28@users.noreply.github.com>
2024-02-15 13:21:18 -0500

    fixes to network infra setup file

--- network.tf
+++ network.tf
@@ -1,7 +1,7 @@
 # Create VPCs and Subnets
 resource "google_compute_network" "vpc" {
   count                           = var.num_vpcs
-  name                            = count.index == 0 ? "vpc" : "${var.vpc_name_prefix}-${count.index}"
+  name                            = count.index == 0 ? var.vpc_name_prefix : "${var.vpc_name_prefix}-${count.index}"
   auto_create_subnetworks         = var.auto_create_subnetworks_flag
   delete_default_routes_on_create = var.delete_default_routes_on_create
   enable_ula_internal_ipv6        = var.enable_ula_internal_ipv6
@@ -9,7 +9,7 @@ resource "google_compute_network" "vpc" {
 }
 resource "google_compute_subnetwork" "public_subnet" {
   count                    = var.num_vpcs
-  name                     = count.index == 0 ? "webapp" : "${var.public_subnet_name_prefix}-${count.index}"
+  name                     = count.index == 0 ? var.public_subnet_name_prefix : "${var.public_subnet_name_prefix}-${count.index}"
   region                   = var.region
   network                  = google_compute_network.vpc[count.index].self_link
   ip_cidr_range            = cidrsubnet(var.public_subnet_cidr_base, 8, count.index)
@@ -18,7 +18,7 @@ resource "google_compute_subnetwork" "public_subnet" {
 }
 resource "google_compute_subnetwork" "private_subnet" {
   count                    = var.num_vpcs
-  name                     = count.index == 0 ? "db" : "${var.private_subnet_name_prefix}-${count.index}"
+  name                     = count.index == 0 ? var.private_subnet_name_prefix : "${var.private_subnet_name_prefix}-${count.index}"
   region                   = var.region
   network                  = google_compute_network.vpc[count.index].self_link
   ip_cidr_range            = cidrsubnet(var.private_subnet_cidr_base, 8, count.index)
@@ -29,7 +29,7 @@ resource "google_compute_subnetwork" "private_subnet" {
 # Add routes to 0.0.0.0/0 for the public subnets resources
 resource "google_compute_route" "public_subnet_route" {
   count            = var.num_vpcs
-  name             = "${var.public_subnet_name_prefix}-${count.index}-route"
+  name             = count.index == 0 ? "${var.public_subnet_name_prefix}-route" :"${var.public_subnet_name_prefix}-${count.index}-route"
   network          = google_compute_network.vpc[count.index].name
   dest_range       = var.route_dest_range
   next_hop_gateway = var.route_next_hop_gateway
